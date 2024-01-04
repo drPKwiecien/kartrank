e228a97b0fece0cb72d0b62cc017022d89820ec7
pkwiecien <przemyslaw.kwiecien@xtb.pl>
2024-01-04 21:37:58 +0100

    dodanie tabeli

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 // Import your custom components
-// import TrackTable from '../components/TrackTable';
+import TrackTable from './components/TrackTable';
 // import GalleryModal from '../components/GalleryModal';
 // import DescriptionPopup from '../components/DescriptionPopup';
 
 const HomePage: React.FC = () => {
     return (
-        <div className="page-container"> {/* Add this wrapper */}
+        <div className="page-container"> 
             <header>
                 <h1>Karting Track Rankings</h1>
             </header>
             <main>
-                <p>Welcome to the karting track ranking website!</p>
-                {/* Placeholder for track table component */}
-                {/* <TrackTable tracks={mockTracks} /> */}
+                <p style={{ marginBottom: '100px' }}>Welcome to the karting track ranking website!</p>
+                <TrackTable />
                 
                 {/* Placeholders for modals/popups */}
                 {/* <GalleryModal isOpen={galleryOpen} onClose={handleGalleryClose} /> */}
